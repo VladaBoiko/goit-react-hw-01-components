6306855edfeae5fc5a2ad5bee5f7884789b3537c
VladaBoiko <bermezha@gmail.com>
2022-09-07 20:40:38 +0300

    add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,15 @@
+import user from '../json/user';
+
+import { ProfileCard } from 'components/Profile/profile';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <ProfileCard
+      username={user.username}
+      tag={user.tag}
+      location={user.location}
+      avatar={user.avatar}
+      stats={user.stats}
+    />
   );
 };
---
+++ src/components/Profile/Profile.styled.jsx
@@ -0,0 +1,68 @@
+import styled from '@emotion/styled';
+export const Profile = styled.div`
+  margin: 50px;
+
+  margin-left: auto;
+  margin-right: auto;
+  width: 320px;
+  border: 1px solid black;
+  background-color: rgb(229, 226, 226);
+`;
+export const Avatar = styled.img`
+  width: 140px;
+  height: 140px;
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+`;
+export const Description = styled.div`
+  padding: 20px;
+`;
+export const Name = styled.p`
+  font-size: 30px;
+  font-weight: 700;
+  text-align: center;
+`;
+export const Tag = styled.p`
+  font-size: 20px;
+  font-weight: 700;
+  text-align: center;
+`;
+export const Location = styled.p`
+  color: rgb(75, 73, 73);
+  font-size: 15px;
+  text-align: center;
+`;
+export const Stats = styled.ul`
+  background-color: rgb(194, 194, 194);
+  display: flex;
+  justify-content: center;
+
+  border-top: 1px solid black;
+  margin: 0;
+  padding: 0;
+  list-style: none;
+`;
+export const Label = styled.span`
+  display: flex;
+  flex-direction: column;
+  color: rgb(41, 38, 54);
+  font-size: 20px;
+`;
+export const Quantity = styled.span`
+  display: block;
+  padding-top: 8px;
+  margin-top: 8px;
+  color: rgb(41, 38, 54);
+  font-size: 18px;
+  font-weight: 700;
+`;
+export const StatsItem = styled.li`
+  padding: 10px;
+  margin: 0;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  color: rgb(75, 73, 73);
+  font-size: 15px;
+`;
